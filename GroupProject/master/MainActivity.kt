565e93692f00fa185e9cd3c4c6808e361a6adf03
Nnamdi Mkpoku <nmkpok1@students.towson.edu>
2023-03-03 22:09:20 -0500

    Add files via upload

---
+++ GroupProject/master/MainActivity.kt
@@ -0,0 +1,20 @@
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+
+
+    }
